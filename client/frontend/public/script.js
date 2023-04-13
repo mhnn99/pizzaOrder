0b2c6375f71886f4a667bb2b53bc010d1a50a5e4
alex <vieru.alexandruu@gmal.com>
2023-04-13 16:53:56 +0300

    partial empty basket

--- client/frontend/public/script.js
+++ client/frontend/public/script.js
@@ -4,29 +4,31 @@ const ulist = document.createElement("ul");
 ulist.classList.add("allergens");
 document.querySelector("#menu-title").appendChild(ulist);
 let cartItems;
-if(localStorage.getItem('cartItems')){
-  cartItems = JSON.parse(localStorage.getItem('cartItems'))
-  console.log(cartItems)
-}else{
-  cartItems=[];
+if (localStorage.getItem("cartItems")) {
+  cartItems = JSON.parse(localStorage.getItem("cartItems"));
+  console.log(cartItems);
+} else {
+  cartItems = [];
 }
 
 let total = 0;
-cartItems.forEach(item=>total+=item.qty)
-console.log(cartItems)
+cartItems.forEach((item) => (total += item.qty));
+console.log(cartItems);
 if (window.location.pathname === "/menu") {
-  document.querySelector('.basket-btn').insertAdjacentHTML('beforeend',`<span>${total}</span>`)
+  document
+    .querySelector(".basket-btn")
+    .insertAdjacentHTML("beforeend", `<span>${total}</span>`);
   const pizzaFetch = async () => {
     const allergenPckgs = await fetch("http://localhost:9000/api/allergens");
     const pizzaPckgs = await fetch("http://localhost:9000/api/pizza");
     const dataAllergens = await allergenPckgs.json();
     const data = await pizzaPckgs.json();
     const pizzaDiv = document.createElement("div");
-      pizzaDiv.setAttribute("class", "row row-cols-1 row-cols-md-3 g-4");
-      document.querySelector("#root").appendChild(pizzaDiv);
-      const pizzaDisplay = (array) => {
-        const pizzaData = array.map(
-          (pizza) => `<div class="col">
+    pizzaDiv.setAttribute("class", "row row-cols-1 row-cols-md-3 g-4");
+    document.querySelector("#root").appendChild(pizzaDiv);
+    const pizzaDisplay = (array) => {
+      const pizzaData = array.map(
+        (pizza) => `<div class="col">
           <div class="card h-100">
           <img src="${pizza.img}" class="card-img-top" alt="...">
           <div class="card-body">
@@ -37,50 +39,67 @@ if (window.location.pathname === "/menu") {
           </div>
           </div>
           </div>`
+      );
+      pizzaDiv.insertAdjacentHTML("beforeend", pizzaData.join(" "));
+      document.querySelectorAll(".card-body").forEach((body) => {
+        const addBtn = document.createElement("button");
+        addBtn.classList.add("add-btn");
+        addBtn.textContent = "Add to cart";
+        body.insertBefore(addBtn, body.children[3]);
+        addBtn.addEventListener("click", (e) => {
+          const index = data.pizzas.findIndex(
+            (pizza) =>
+              pizza.name === e.target.parentNode.children[0].textContent
           );
-          pizzaDiv.insertAdjacentHTML("beforeend", pizzaData.join(" "));
-          document.querySelectorAll('.card-body').forEach(body=>{
-          const addBtn = document.createElement('button')
-          addBtn.classList.add('add-btn')
-          addBtn.textContent = 'Add to cart'
-          body.insertBefore(addBtn,body.children[3])
-          addBtn.addEventListener('click',(e)=>{
-            const index = data.pizzas.findIndex(pizza=>pizza.name===e.target.parentNode.children[0].textContent)
-            const index2 = cartItems.findIndex(cart=>cart.name===data.pizzas[index].name)
-              if(index2!==-1){
-              cartItems.splice(index2, 1, {...data.pizzas[index], qty:Number(cartItems[index2].qty) + Number(e.target.nextSibling.value)})
-              }else{
-                cartItems.push({...data.pizzas[index], qty:Number(e.target.nextSibling.value)})
-              }
-              total+=Number(e.target.nextSibling.value)
-              document.querySelector('span').textContent = Number(document.querySelector('span').textContent) + Number(e.target.nextSibling.value)
-            localStorage.setItem('cartItems', JSON.stringify(cartItems))
-            console.log(cartItems)
-          })
-        })
-      }
-      pizzaDisplay(data.pizzas)
-      dataAllergens.forEach((allergen) => {
-        const allergenDiv = document.createElement("li");
-        allergenDiv.classList.add("allergen");
-        allergenDiv.innerHTML = `
+          const index2 = cartItems.findIndex(
+            (cart) => cart.name === data.pizzas[index].name
+          );
+          if (index2 !== -1) {
+            cartItems.splice(index2, 1, {
+              ...data.pizzas[index],
+              qty:
+                Number(cartItems[index2].qty) +
+                Number(e.target.nextSibling.value),
+            });
+          } else {
+            cartItems.push({
+              ...data.pizzas[index],
+              qty: Number(e.target.nextSibling.value),
+            });
+          }
+          total += Number(e.target.nextSibling.value);
+          document.querySelector("span").textContent =
+            Number(document.querySelector("span").textContent) +
+            Number(e.target.nextSibling.value);
+          localStorage.setItem("cartItems", JSON.stringify(cartItems));
+          console.log(cartItems);
+        });
+      });
+    };
+    pizzaDisplay(data.pizzas);
+    dataAllergens.forEach((allergen) => {
+      const allergenDiv = document.createElement("li");
+      allergenDiv.classList.add("allergen");
+      allergenDiv.innerHTML = `
                 <li>${allergen.id}. ${allergen.name}</li>
                 `;
-        ulist.appendChild(allergenDiv);
+      ulist.appendChild(allergenDiv);
+    });
+    document.querySelectorAll(".allergen").forEach((allergen) => {
+      allergen.addEventListener("click", (e) => {
+        const index = dataAllergens.findIndex(
+          (allergen) => allergen.name === e.target.innerHTML.split(" ")[1]
+        );
+        const pizzaData = data.pizzas.filter((pizza) =>
+          pizza.allergens.includes(dataAllergens[index].id)
+        );
+        pizzaDiv.innerHTML = "";
+        pizzaDisplay(pizzaData);
+        console.log(dataAllergens[index].id);
       });
-      document.querySelectorAll(".allergen").forEach(allergen => {
-        allergen.addEventListener('click', (e) => {
-
-          const index = dataAllergens.findIndex(allergen => allergen.name === e.target.innerHTML.split(' ')[1])
-          const pizzaData = data.pizzas.filter(pizza => pizza.allergens.includes(dataAllergens[index].id))
-          pizzaDiv.innerHTML = ''
-          pizzaDisplay(pizzaData)
-          console.log(dataAllergens[index].id)  
-        })
-
-      })
-  }
-  pizzaFetch()
+    });
+  };
+  pizzaFetch();
 } else if (window.location.pathname === "/about") {
   document.querySelector("#root").insertAdjacentHTML(
     "beforeend",
@@ -192,19 +211,21 @@ if (window.location.pathname === "/menu") {
 </section>`
   );
 } else if (window.location.pathname === "/basket") {
-  if(cartItems.length){
+  if (cartItems.length) {
     const priceReducer = (items, flag) => {
       const totalPrice = items.reduce((total, pizza) => {
-        if(flag === 'price'){
-          total += pizza.price*pizza.qty
-        }else{
-          total += pizza.qty
+        if (flag === "price") {
+          total += pizza.price * pizza.qty;
+        } else {
+          total += pizza.qty;
         }
-        return total
-      },0)
-      return totalPrice
-    }
-    document.querySelector('#root').insertAdjacentHTML('beforeend',`<div class="card">
+        return total;
+      }, 0);
+      return totalPrice;
+    };
+    document.querySelector("#root").insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
     <div class="row">
         <div class="col-lg-8 cart">
             <div class="title">
@@ -227,12 +248,17 @@ if (window.location.pathname === "/menu") {
             </form>
             <div class="row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
                 <div class="col">TOTAL PRICE</div>
-                <div id="total-price" class="col text-right">$${priceReducer(cartItems, 'price')}</div>
+                <div id="total-price" class="col text-right">$${priceReducer(
+                  cartItems,
+                  "price"
+                )}</div>
                 </div>
             </div>
-            </div> `)
+            </div> `
+    );
 
-    const itemDetails = cartItems.map(item=>`<div class="row border-top border-bottom">
+    const itemDetails = cartItems.map(
+      (item) => `<div class="row border-top border-bottom">
     <div class="row main align-items-center">
         <div class="col-2"><img class="img-fluid" src=${item.img}></div>
         <div class="col">
@@ -240,66 +266,138 @@ if (window.location.pathname === "/menu") {
             <div class="row">${item.name}</div>
         </div>
         <div class="col">
-            <a href="#" id='substract'>-</a><a href="#" class="border">${item.qty}</a><a href="#" id='add'>+</a>
+            <a href="#" id='substract'>-</a><a href="#" class="border">${
+              item.qty
+            }</a><a href="#" id='add'>+</a>
         </div>
-        <div class="col">$${item.price * item.qty} <span class="close">&#10005;</span></div>
+        <div class="col">$${
+          item.price * item.qty
+        } <span class="close">&#10005;</span></div>
     </div>
-  </div>`)
-  document.querySelector('.cart').insertAdjacentHTML('beforeend',itemDetails.join(' '))
-  document.querySelectorAll('#substract').forEach(btn=>btn.addEventListener('click',(e)=>{
-    const index = cartItems.findIndex(item=>item.name===e.target.parentNode.parentNode.children[1].children[1].textContent)
-    if(cartItems[index].qty>1){
-    cartItems[index].qty--}
-    else{
-      cartItems.splice(index,1)
-      e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode)
-    }
-    localStorage.setItem('cartItems', JSON.stringify(cartItems))
-    e.target.nextSibling.textContent = cartItems[index].qty
-    e.target.parentNode.nextSibling.nextSibling.firstChild.textContent = `$${cartItems[index].qty * cartItems[index].price}`
-    document.querySelector("#total-items").innerHTML = `${priceReducer(cartItems, 'qty')} items`
-    document.querySelector("#total-price").innerHTML = `$${priceReducer(cartItems, 'price')}`
-    console.log(cartItems)
-  }))
-  document.querySelectorAll('#add').forEach(btn=>btn.addEventListener('click',(e)=>{
-    const index = cartItems.findIndex(item=>item.name===e.target.parentNode.parentNode.children[1].children[1].textContent)
-    cartItems[index].qty++
-    e.target.previousSibling.textContent = cartItems[index].qty
-    e.target.parentNode.nextSibling.nextSibling.firstChild.textContent = `$${cartItems[index].qty * cartItems[index].price}`
-    localStorage.setItem('cartItems', JSON.stringify(cartItems))
-    document.querySelector("#total-items").innerHTML = `${priceReducer(cartItems, 'qty')} items`
-    document.querySelector("#total-price").innerHTML = `$${priceReducer(cartItems, 'price')}`
-    console.log(cartItems)
-  }))
-  document.querySelectorAll('.close').forEach(btn=>btn.addEventListener('click', (e) =>{
-    const index = cartItems.findIndex(item=>item.name===e.target.parentNode.parentNode.children[1].children[1].textContent)
-    cartItems.splice(index,1)
-      e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode)
-      console.log(cartItems)
-      localStorage.setItem('cartItems', JSON.stringify(cartItems))
-  }))
+  </div>`
+    );
+    document
+      .querySelector(".cart")
+      .insertAdjacentHTML("beforeend", itemDetails.join(" "));
+    document.querySelectorAll("#substract").forEach((btn) =>
+      btn.addEventListener("click", (e) => {
+        const index = cartItems.findIndex(
+          (item) =>
+            item.name ===
+            e.target.parentNode.parentNode.children[1].children[1].textContent
+        );
+        if (cartItems[index].qty > 1) {
+          cartItems[index].qty--;
+        } else {
+          cartItems.splice(index, 1);
+          e.target.parentNode.parentNode.parentNode.removeChild(
+            e.target.parentNode.parentNode
+          );
+        }
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
+        e.target.nextSibling.textContent = cartItems[index].qty;
+        e.target.parentNode.nextSibling.nextSibling.firstChild.textContent = `$${
+          cartItems[index].qty * cartItems[index].price
+        }`;
+        document.querySelector("#total-items").innerHTML = `${priceReducer(
+          cartItems,
+          "qty"
+        )} items`;
+        document.querySelector("#total-price").innerHTML = `$${priceReducer(
+          cartItems,
+          "price"
+        )}`;
+        console.log(cartItems);
+      })
+    );
+    document.querySelectorAll("#add").forEach((btn) =>
+      btn.addEventListener("click", (e) => {
+        const index = cartItems.findIndex(
+          (item) =>
+            item.name ===
+            e.target.parentNode.parentNode.children[1].children[1].textContent
+        );
+        cartItems[index].qty++;
+        e.target.previousSibling.textContent = cartItems[index].qty;
+        e.target.parentNode.nextSibling.nextSibling.firstChild.textContent = `$${
+          cartItems[index].qty * cartItems[index].price
+        }`;
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
+        document.querySelector("#total-items").innerHTML = `${priceReducer(
+          cartItems,
+          "qty"
+        )} items`;
+        document.querySelector("#total-price").innerHTML = `$${priceReducer(
+          cartItems,
+          "price"
+        )}`;
+        console.log(cartItems);
+      })
+    );
+    document.querySelectorAll(".close").forEach((btn) =>
+      btn.addEventListener("click", (e) => {
+        const index = cartItems.findIndex(
+          (item) =>
+            item.name ===
+            e.target.parentNode.parentNode.children[1].children[1].textContent
+        );
+        cartItems.splice(index, 1);
+        e.target.parentNode.parentNode.parentNode.removeChild(
+          e.target.parentNode.parentNode
+        );
+        console.log(cartItems);
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
+      })
+    );
+  } else {
+    document.querySelector("#root").insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
+  <div class="row">
+      <div class="col-lg-8 cart">
+          <div class="title">
+              <div class="row">
+                  <div class="col"><h4><b>Shopping Cart</b></h4></div>
+                  <div id="total-items" class="col align-self-center text-right text-muted"></div>
+                  <div id="go-back-msg">Your shopping cart is empty</div>
+                  <svg class="pl" width="128px" height="128px" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
+                    <circle class="pl__ring1" cx="64" cy="64" r="60" fill="none" stroke="hsl(3,90%,55%)" stroke-width="8" transform="rotate(-90,64,64)" stroke-linecap="round" stroke-dasharray="377 377" stroke-dashoffset="-376.4"></circle>
+                    <circle class="pl__ring2" cx="64" cy="64" r="52.5" fill="none" stroke="hsl(13,90%,55%)" stroke-width="7" transform="rotate(-90,64,64)" stroke-linecap="round" stroke-dasharray="329.9 329.9" stroke-dashoffset="-329.3"></circle>
+                    <circle class="pl__ring3" cx="64" cy="64" r="46" fill="none" stroke="hsl(23,90%,55%)" stroke-width="6" transform="rotate(-90,64,64)" stroke-linecap="round" stroke-dasharray="289 289" stroke-dashoffset="-288.6"></circle>
+                    <circle class="pl__ring4" cx="64" cy="64" r="40.5" fill="none" stroke="hsl(33,90%,55%)" stroke-width="5" transform="rotate(-90,64,64)" stroke-linecap="round" stroke-dasharray="254.5 254.5" stroke-dashoffset="-254"></circle>
+                    <circle class="pl__ring5" cx="64" cy="64" r="36" fill="none" stroke="hsl(43,90%,55%)" stroke-width="4" transform="rotate(-90,64,64)" stroke-linecap="round" stroke-dasharray="226.2 226.2" stroke-dashoffset="-225.8"></circle>
+                    <circle class="pl__ring6" cx="64" cy="64" r="32.5" fill="none" stroke="hsl(53,90%,55%)" stroke-width="3" transform="rotate(-90,64,64)" stroke-linecap="round" stroke-dasharray="204.2 204.2" stroke-dashoffset="-203.9"></circle>
+                  </svg>
+                  </div>
+                  <button class="back-btN">
+                    <span> Back to Menu</span>
+                  </button>             
+          </div>
+      </div>
+      
+    </div> `
+    );
+    document.querySelector(".back-btN").addEventListener("click", () => {
+      window.location.pathname = "/menu";
+    });
   }
-  const form = document.querySelector('form')
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
+}
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-      const formData = new FormData(e.target);
-      const entries = [...formData.entries()];
-  
-      const employee = entries.reduce((acc, entry) => {
-        const [k, v] = entry;
-        acc[k] = v;
-        return acc;
-      }, {});
+  const formData = new FormData(e.target);
+  const entries = [...formData.entries()];
 
-  console.log(employee)
+  const employee = entries.reduce((acc, entry) => {
+    const [k, v] = entry;
+    acc[k] = v;
+    return acc;
+  }, {});
 
+  console.log(employee);
 
-// for (const [key, value] of formData) {
-//   output.textContent += ${key}: ${value}\n;
-// }
-    
-  })
-  }else{
-    
-  }
+  // for (const [key, value] of formData) {
+  //   output.textContent += ${key}: ${value}\n;
+  // }
+});
